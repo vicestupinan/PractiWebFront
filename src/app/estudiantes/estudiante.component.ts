b977d8b7f460ff83c7ee93bd45d05a3aef4d0bf6
vicestupinan <victor.estupinan96@gmail.com>
2022-06-04 19:16:17 -0500

    .

--- src/app/estudiantes/estudiante.component.ts
+++ src/app/estudiantes/estudiante.component.ts
@@ -1,7 +1,10 @@
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Estudiante } from '../models/estudiante';
 import { EstudianteService } from '../services/estudiante.service';
 import { TokenService } from '../services/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-students',
@@ -17,10 +20,16 @@ export class StudentsComponent implements OnInit {
   public pageSize = 5;
   public lenght = 0;
 
+  selectedFiles?: FileList;
+  currentFile?: File;
+  progress = 0;
+  message = '';
+  fileInfos?: Observable<any>;
+
   constructor(
     private estudianteService: EstudianteService,
-    private tokenService: TokenService
-
+    private tokenService: TokenService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -38,4 +47,47 @@ export class StudentsComponent implements OnInit {
       }
     );
   }
+
+  selectFile(event: any) {
+    this.selectedFiles = event.target.files;
+  }
+
+  upload(): void {
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+
+      if (file) {
+        this.currentFile = file;
+
+        this.estudianteService.subir(this.currentFile).subscribe(
+          (event: any) => {
+            if (event.type === HttpEventType.UploadProgress) {
+              this.progress = Math.round(100 * event.loaded / event.total);
+              this.ngOnInit();
+            } else if (event instanceof HttpResponse) {
+              this.message = event.body.message;
+              this.fileInfos = this.estudianteService.lista();
+            }
+          },
+          (err: any) => {
+            console.log(err);
+            this.progress = 0;
+
+            if (err.error && err.error.message) {
+              this.message = err.error.message;
+            } else {
+              this.message = 'Could not upload the file!';
+            }
+
+            this.currentFile = undefined;
+          });
+          setTimeout(
+            function(){ 
+              window.location.reload();
+            }, 2000);
+      }
+      this.selectedFiles = undefined;
+    }
+  }
+
 }
--- src/app/services/estudiante.service.ts
+++ src/app/services/estudiante.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Estudiante } from '../models/estudiante';
@@ -14,6 +14,19 @@ export class EstudianteService {
 
   public lista(): Observable<Estudiante[]>{
     return this.httpClient.get<Estudiante[]>(this.estudianteURL + 'lista');
+  }
+
+  public subir(file: File): Observable<HttpEvent<any>>{
+    const formData: FormData = new FormData();
+	
+		formData.append('file', file);
+	
+		const req = new HttpRequest('POST', this.estudianteURL + 'subir', formData, {
+		  reportProgress: true,
+		  responseType: 'json'
+		});
+	
+		return this.httpClient.request(req);
   }
 
 }
